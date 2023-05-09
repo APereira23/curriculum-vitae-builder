4d7c3408d3b37568639dee4f31da6199769cce6c
AntonioMPereira <antoniofmpereira@gmail.com>
2023-05-09 22:22:16 +0100

    deleted personal info form template

--- src/data-eng.js
+++ src/data-eng.js
@@ -5,20 +5,20 @@ const bio = [
     title: "Personal Information",
     id: uniqid(),
     contents: {
-      Name: "António Flávio Marques Pereira",
-      Email: "antoniofmpereira@gmail.com",
-      Phone: "+351 928154529",
-      City: "Lisbon, Portugal",
-      LinkedIn: "https://bit.ly/4220rhj",
+      Name: "Name",
+      Email: "email@email.com",
+      Phone: "number",
+      City: "city",
+      LinkedIn: "link",
     }
   },
   {
     title: "Languages",
     id: uniqid(),
     contents: [
-      "Portuguese (native)",
-      "English (C2)",
-      "German (A2)",    
+      "lang1",
+      "lang2",
+      "lang3",    
     ]
   },
   {
@@ -40,33 +40,10 @@ const bio = [
 
 const work = [
   {
-    title: "Team Coordinator / Administrative Assistant",
-    when: "september 2020 - august 2022",
-    where: "EXPLICOLÂNDIA Study Center (Lisboa-Alvalade)",
+    title: "title",
+    when: "when",
+    where: "where",
     tasks: [ //each task as an array string
-      "Manage work schedules;",
-      "Manage information flow between parents, students and their respective tutor;",
-      "Handle enrollments and customer service;",
-      "Process payroll sheets;",
-      "Handle recruitment of new tutors.",
-    ],
-    id: uniqid(),
-  },
-  {
-    title: "Tutor",
-    when: "march 2019 - june 2020; september 2022 - april 2023" ,
-    where: "EXPLICOLÂNDIA Study Center (Lisboa-Alvalade)",
-    tasks: [
-      "Teach Portuguese, English and Philosophy classes;",
-      "Prepare high-school students for their final exams."
-    ],
-    id: uniqid(),
-  },
-  {
-    title: "Cashier",
-    when: "august 2017 - january 2018",
-    where: "Continente Bom Dia (Saldanha, Lisboa)",
-    tasks: [
     ],
     id: uniqid(),
   },
@@ -74,18 +51,11 @@ const work = [
 
 const education = [
   {
-    title: "Masters degree",
-    course: "Literary Theory",
-    when: "2017-2020",
-    where: "University of Lisbon's School of Arts and Humanities",
-    link: "http://hdl.handle.net/10451/50853",
-    id: uniqid(),
-  },
-  {
-    title: "Bachelor's Degree",
-    course: "Languages, Literatures and Cultures",
-    when: "2014 - 2017",
-    where: "University of Lisbon's School of Arts and Humanities",
+    title: "title",
+    course: "course",
+    when: "when",
+    where: "where",
+    link: "link.com",
     id: uniqid(),
   },
 ];
@@ -112,16 +82,12 @@ const skills = [
 const hobbies = [
   {
     id: uniqid(),
-    text: "Musical studies (guitar and composition);",
+    text: "hobby1;",
   },
   {
     id: uniqid(),
-    text: "Writing (published several articles and papers on many academic publications (literary studies / reviews and other culture-related articles);"
+    text: "hobby2;"
   },
-  {
-    id: uniqid(),
-    text: "Computer Science."
-  }
 ];
 
 
